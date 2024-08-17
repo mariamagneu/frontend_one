434988ab81251d0978e3ebef674b7733d1d86dcb
maria magdalena <neumariamagdalena@gmail.com>
2024-08-17 21:26:25 +0200

    basic setup for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MantineProvider } from '@mantine/core'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css'
-import './styles/global.css'
-import theme from './styles/theme.js'
+import "./styles/global.css";
+import theme from "./styles/theme";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>
-)
+);
