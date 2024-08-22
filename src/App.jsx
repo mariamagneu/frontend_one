1f8e386e80178e05cc0a3f16bc01360e13434a9b
maria magdalena <neumariamagdalena@gmail.com>
2024-08-22 20:58:45 +0200

    add login page to app.jsx and import SessionContextProvider in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
         </Routes>
       </main>
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import SessionContextProvider from "./contexts/SessionContext";
 
 import "./styles/global.css";
 import theme from "./styles/theme";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <SessionContextProvider>
+          <App />
+        </SessionContextProvider>
       </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>
